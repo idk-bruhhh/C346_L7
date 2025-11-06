38ce3fa538e617a7757cb72ae791a99042c28ede
QiYan <24030663@myrp.edu.sg>
2025-11-06 12:48:41 +0800

    Initial commit

---
+++ lesson2/L2/AppBackup.js
@@ -0,0 +1,38 @@
+// import React from 'react';
+// import {View, Text} from 'react-native';
+// const Greeting = ( {addr,name})=> {
+//     return (
+//         <View>
+//             <Text>
+//                 Hello, {addr} {name}!
+//             </Text>
+//         </View>
+//     );
+// };
+// // export default Greeting;
+//
+// // const ThreeGreet = () => {
+// //     return (
+// //         <View>
+// //             <Text></Text>
+// //             <Greeting name="QiYan" />
+// //             <Greeting name=""/>
+// //             <Greeting name=""/>
+// //         </View>
+// //     );
+// // };
+//
+// class ThreeGreet extends React.Component {
+//     render() {App.js
+//         return (
+//             <View>
+//                 <Text></Text>
+//                 <Greeting addr="Mr" name="Qi Yan"/>
+//                 <Greeting addr="Miss" name="Auni"/>
+//                 <Greeting addr="Miss"  name="Xin Fang"/>
+//             </View>
+//         )
+//     }
+// }
+//
+// export default ThreeGreet;
